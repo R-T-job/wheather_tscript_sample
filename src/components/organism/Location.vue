164cddf89a63c8d729f20acb204aa4d260debff6
rtjob <3rd103@gmail.com>
2020-09-11 08:13:27 +0900

    update location component api

--- src/components/organism/Location.vue
+++ src/components/organism/Location.vue
@@ -6,13 +6,26 @@
 </template>
 <script lang="ts">
   import { defineComponent } from "@vue/composition-api";
-  import {Component, Prop, Watch, Vue} from "vue-property-decorator";  
+  import {Component, Prop, Watch, Vue} from "vue-property-decorator";
 
-  @Component
-  export default class Location extends Vue {
-    @Prop() public locationTimezone!: string;
-    @Prop() public wheatherIconSrc!: string;
-  }
+  type LocationProp = {
+    locationTimezone: string
+    wheatherIconSrc: string
+  };
+  
+  export default defineComponent({
+    props: {
+      locationTimezone: {
+        type: String,
+        default: ""
+      },
+      wheatherIconSrc: {
+        type: String,
+        default: ""
+      },
+    },
+  });
+
 </script>
 <style>
 .location {
